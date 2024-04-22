73ccd250671203d2b876e99de59b16615b480f04
ReinstatedSamit <69500663+ReinstatedSamit@users.noreply.github.com>
2024-04-22 11:02:49 -0400

    Update Lifetime_algo.py

--- Lifetime_algo.py
+++ Lifetime_algo.py
@@ -5,7 +5,7 @@ def ComputeEnergyToSendData(sa, MaxDataPacketSize, PTx, PRx, PIdle, PSleep, PER,
     Compute the energy consumed to send data.
 
     Args:
-    sa (int): Size of data in bytes.
+    sa (int): Size of application data in bytes.
     MaxDataPacketSize (int): Maximum size of a data packet in bytes.
     PTx (float): Power consumption during transmission in watts.
     PRx (float): Power consumption during reception in watts.
@@ -143,44 +143,221 @@ def Lifetime_cal(ta, tsyn, sa, MaxDataPacketSize, PTx, PRx, PIdle, PSleep, PER, 
         Lifetime = Lifetime + ta
     return Lifetime
 
-ta_list_real = [1, 10, 100, 1000, 10000, 100000, 1000000] # Total time in seconds
-ta_list = [100]
-total_time_inyear_list=[]
-# Example usage:
+sa_list = [0.008,0.08,0.8,8,80,800, 8000, 80000, 800000, 8000000] # Total time in seconds
+ta_list = [100, 1000, 10000, 100000, 1000000, 10000000]
+ta_list_one = [3600*24]
+total_time_inyear_list_loRA=[]
+total_time_inyear_list_BLE=[]
+total_time_inyear_list_NBIoT=[]
+
+# Example usage for BLE
 for i in range(len(ta_list)):
     print('ta',ta_list[i])
     tsyn = 5 * 365 * 24 * 60 * 60  # Time for synchronization in seconds
-    sa = 800  # Size of data in bytes
+    sa = 0.8  # Size of data in bytes
     PER = 0.1  # Packet error rate
-    MaxDataPacketSize = 245  # Maximum size of a data packet in bytes
-    PTx = 420e-3#24e-3  # Power consumption
-    PRx = 44e-3#20e-3  # Power consumption
-    PIdle = 0#5e-3  # Power consumption
-    PSleep = 0  # 3e-6  # Power consumption
-    tsTx = (2193e-3)*245/100#1080e-6  # Transmission time in seconds
-    tsRx = 400e-6#400e-6  # Reception time in seconds
-    tsIdle = 0#1080e-6  # Idle time in seconds
+    MaxDataPacketSize = 245#(Lora)#(NBIOIT)#245(BLE/LoRa)  # Maximum size of a data packet in bytes
+    PTx = 24e-3#(BLE)  # Power consumption
+    PRx = 20e-3#(BLE)  # Power consumption
+    PIdle =5e-3 # (BLE)# Power consumption
+    PSleep =3e-6#(BLE)  # Power consumption
+    tsTx = 1080e-6#(BLE)#(2193e-3)*245/100#1080e-6(BLE)  # Transmission time in seconds
+    tsRx = 400e-6#(BLE)#400e-6#400e-6  (BLE)# Reception time in seconds
+    tsIdle = 1080e-6  # Idle time in seconds
     ton = tsTx + tsRx + tsIdle
     tsSleep = ta_list[i] - ton  # Sleep time in seconds
     print(tsSleep)
     PER = 0.01  # Packet error rate
-    PSleepR = 4.3e-6#3e-6  # Power consumption during sleep mode for reception in watts
+    PSleepR = 0#0.001408e-3#4.3e-6#3e-6  # Power consumption during sleep mode for reception in watts
     synchronization_on_data_frame_possible = 0  # Boolean indicating if synchronization on data frame is possible
     Esyn = 0  # Energy consumed for synchronization in joules
     Ebattery = 13000  # Initial energy level of the battery in joules
     total_time = Lifetime_cal(ta_list[i], tsyn, sa, MaxDataPacketSize, PTx, PRx, PIdle, PSleep, PER, tsTx, tsRx, tsIdle,
                               tsSleep, ton, PSleepR, synchronization_on_data_frame_possible, Esyn, Ebattery)
-    total_time_inyear= total_time / (3600 * 24 * 365)
-    print(
-        f"Total time in {'year' if total_time_inyear >= 1 else 'day'}s: {total_time_inyear if total_time_inyear >= 1 else total_time / (3600 * 24)}")
-    total_time_inyear_list.append(total_time_inyear)
+    total_time_inyear_BLE= total_time / (3600 * 24 * 365)
+    #print(
+        #f"Total time in {'year' if total_time_inyear >= 1 else 'day'}s: {total_time_inyear if total_time_inyear >= 1 else total_time / (3600 * 24)}")
+    total_time_inyear_list_BLE.append(total_time_inyear_BLE)
 
 
-print("ta_list =", ta_list)
-print("total_time_year_list =", total_time_inyear_list)
 
-"""plt.semilogx(ta_list, total_time_inyear_list)
-plt.xlabel('ta_list in seconds')
+# Example usage for LORA
+for i in range(len(ta_list)):
+    print('ta',ta_list[i])
+    tsyn = 5 * 365 * 24 * 60 * 60  # Time for synchronization in seconds
+    sa = 0.8  # Size of data in bytes
+    PER = 0.1  # Packet error rate
+    MaxDataPacketSize = 250#(Lora)#(NBIOIT)#245(BLE/LoRa)  # Maximum size of a data packet in bytes
+    PTx = 420e-3#24e-3(BLE)  # Power consumption
+    PRx = 44e-3#20e-3(BLE)  # Power consumption
+    PIdle = 7e-3#5e-3  (BLE)# Power consumption
+    PSleep = 0.001408e-3  # 3e-6(BLE)  # Power consumption
+    tsTx = 7755093e-6#(Lora)#(2193e-3)*245/100#1080e-6(BLE)  # Transmission time in seconds
+    tsRx = 7755093e-6#(Lora)#400e-6#400e-6  (BLE)# Reception time in seconds
+    tsIdle = 1#1080e-6  # Idle time in seconds
+    ton = tsTx + tsRx + tsIdle
+    tsSleep = ta_list[i] - ton  # Sleep time in seconds
+    print(tsSleep)
+    PER = 0.01  # Packet error rate
+    PSleepR = 0#0.001408e-3#4.3e-6#3e-6  # Power consumption during sleep mode for reception in watts
+    synchronization_on_data_frame_possible = 0  # Boolean indicating if synchronization on data frame is possible
+    Esyn = 0  # Energy consumed for synchronization in joules
+    Ebattery = 13000  # Initial energy level of the battery in joules
+    total_time = Lifetime_cal(ta_list[i], tsyn, sa, MaxDataPacketSize, PTx, PRx, PIdle, PSleep, PER, tsTx, tsRx, tsIdle,
+                              tsSleep, ton, PSleepR, synchronization_on_data_frame_possible, Esyn, Ebattery)
+    total_time_inyear_lora= total_time / (3600 * 24 * 365)
+   # print(
+     #   f"Total time in {'year' if total_time_inyear >= 1 else 'day'}s: {total_time_inyear if total_time_inyear >= 1 else total_time / (3600 * 24)}")
+    total_time_inyear_list_loRA.append(total_time_inyear_lora)
+
+# Example usage for NBIOT
+for i in range(len(ta_list)):
+    print('ta',ta_list[i])
+    tsyn = 5 * 365 * 24 * 60 * 60  # Time for synchronization in seconds
+    sa = 0.8  # Size of data in bytes
+    PER = 0.1  # Packet error rate
+    MaxDataPacketSize = 1500#(Lora)#(NBIOIT)#245(BLE/LoRa)  # Maximum size of a data packet in bytes
+    PTx = 543e-3#24e-3(BLE)  # Power consumption
+    PRx = 90e-3#20e-3(BLE)  # Power consumption
+    PIdle = 2.4e-3#5e-3  (BLE)# Power consumption
+    PSleep = 0.015e-3  # 3e-6(BLE)  # Power consumption
+    tsTx = 1.67#0.92857e-3#(Lora)#(2193e-3)*245/100#1080e-6(BLE)  # Transmission time in seconds
+    tsRx = 0.92857e-3#(Lora)#400e-6#400e-6  (BLE)# Reception time in seconds
+    tsIdle = 10#1080e-6  # Idle time in seconds
+    ton = tsTx + tsRx + tsIdle
+    tsSleep = ta_list[i] - ton  # Sleep time in seconds
+    print(tsSleep)
+    PER = 0.01  # Packet error rate
+    PSleepR = 0#0.001408e-3#4.3e-6#3e-6  # Power consumption during sleep mode for reception in watts
+    synchronization_on_data_frame_possible = 0  # Boolean indicating if synchronization on data frame is possible
+    Esyn = 0  # Energy consumed for synchronization in joules
+    Ebattery = 13000  # Initial energy level of the battery in joules
+    total_time = Lifetime_cal(ta_list[i], tsyn, sa, MaxDataPacketSize, PTx, PRx, PIdle, PSleep, PER, tsTx, tsRx, tsIdle,
+                              tsSleep, ton, PSleepR, synchronization_on_data_frame_possible, Esyn, Ebattery)
+    total_time_inyear_NBIOT= total_time / (3600 * 24 * 365)
+   # print(
+     #   f"Total time in {'year' if total_time_inyear >= 1 else 'day'}s: {total_time_inyear if total_time_inyear >= 1 else total_time / (3600 * 24)}")
+    total_time_inyear_list_NBIoT.append(total_time_inyear_NBIOT)
+
+
+plt.semilogx(ta_list, total_time_inyear_list_BLE, label="BLE")
+plt.semilogx(ta_list, total_time_inyear_list_loRA, label="LoRa")
+plt.semilogx(ta_list, total_time_inyear_list_NBIoT, label="NBIOT")
+plt.xlabel('ta_list in sec sa=0.8 b/s')
 plt.ylabel('total_time_year_list')
+plt.legend()
 plt.grid(True)
-plt.show()"""
+plt.show()
+
+
+
+'''for i in range(len(sa_list)):
+    print('sa',sa_list[i])
+    tsyn = 5 * 365 * 24 * 60 * 60  # Time for synchronization in seconds
+    ta = 24*3600  # Size of data in bytes
+    PER = 0.1  # Packet error rate
+    MaxDataPacketSize = 245#(Lora)#(NBIOIT)#245(BLE/LoRa)  # Maximum size of a data packet in bytes
+    PTx = 24e-3#(BLE)  # Power consumption
+    PRx = 20e-3#(BLE)  # Power consumption
+    PIdle =5e-3 # (BLE)# Power consumption
+    PSleep =3e-6#(BLE)  # Power consumption
+    tsTx = 1080e-6#(BLE)#(2193e-3)*245/100#1080e-6(BLE)  # Transmission time in seconds
+    tsRx = 400e-6#(BLE)#400e-6#400e-6  (BLE)# Reception time in seconds
+    tsIdle = 1080e-6  # Idle time in seconds
+    ton = tsTx + tsRx + tsIdle
+    tsSleep = ta - ton  # Sleep time in seconds
+    print(tsSleep)
+    PER = 0.01  # Packet error rate
+    PSleepR = 0#0.001408e-3#4.3e-6#3e-6  # Power consumption during sleep mode for reception in watts
+    synchronization_on_data_frame_possible = 0  # Boolean indicating if synchronization on data frame is possible
+    Esyn = 0  # Energy consumed for synchronization in joules
+    Ebattery = 13000  # Initial energy level of the battery in joules
+    total_time = Lifetime_cal(ta, tsyn, sa_list[i], MaxDataPacketSize, PTx, PRx, PIdle, PSleep, PER, tsTx, tsRx, tsIdle,
+                              tsSleep, ton, PSleepR, synchronization_on_data_frame_possible, Esyn, Ebattery)
+    total_time_inyear_BLE= total_time / (3600 * 24 * 365)
+    #print(
+        #f"Total time in {'year' if total_time_inyear >= 1 else 'day'}s: {total_time_inyear if total_time_inyear >= 1 else total_time / (3600 * 24)}")
+    total_time_inyear_list_BLE.append(total_time_inyear_BLE)
+
+
+
+# Example usage:
+for i in range(len(sa_list)):
+    print('sa',sa_list[i])
+    tsyn = 5 * 365 * 24 * 60 * 60  # Time for synchronization in seconds
+    ta = 24*3600  # Size of data in bytes
+    PER = 0.1  # Packet error rate
+    MaxDataPacketSize = 250#(Lora)#(NBIOIT)#245(BLE/LoRa)  # Maximum size of a data packet in bytes
+    PTx = 420e-3#24e-3(BLE)  # Power consumption
+    PRx = 44e-3#20e-3(BLE)  # Power consumption
+    PIdle = 7e-3#5e-3  (BLE)# Power consumption
+    PSleep = 0.001408e-3  # 3e-6(BLE)  # Power consumption
+    tsTx = 7755093e-6#(Lora)#(2193e-3)*245/100#1080e-6(BLE)  # Transmission time in seconds
+    tsRx = 7755093e-6#(Lora)#400e-6#400e-6  (BLE)# Reception time in seconds
+    tsIdle = 1#1080e-6  # Idle time in seconds
+    ton = tsTx + tsRx + tsIdle
+    tsSleep = ta - ton  # Sleep time in seconds
+    print(tsSleep)
+    PER = 0.01  # Packet error rate
+    PSleepR = 0#0.001408e-3#4.3e-6#3e-6  # Power consumption during sleep mode for reception in watts
+    synchronization_on_data_frame_possible = 0  # Boolean indicating if synchronization on data frame is possible
+    Esyn = 0  # Energy consumed for synchronization in joules
+    Ebattery = 13000  # Initial energy level of the battery in joules
+    total_time = Lifetime_cal(ta, tsyn, sa_list[i], MaxDataPacketSize, PTx, PRx, PIdle, PSleep, PER, tsTx, tsRx, tsIdle,
+                              tsSleep, ton, PSleepR, synchronization_on_data_frame_possible, Esyn, Ebattery)
+    total_time_inyear_lora= total_time / (3600 * 24 * 365)
+   # print(
+     #   f"Total time in {'year' if total_time_inyear >= 1 else 'day'}s: {total_time_inyear if total_time_inyear >= 1 else total_time / (3600 * 24)}")
+    total_time_inyear_list_loRA.append(total_time_inyear_lora)
+
+# Example usage:
+for i in range(len(sa_list)):
+    print('sa',sa_list[i])
+    tsyn = 5 * 365 * 24 * 60 * 60  # Time for synchronization in seconds
+    ta = 24*3600  # Size of data in bytes
+    PER = 0.1  # Packet error rate
+    MaxDataPacketSize = 1500#(Lora)#(NBIOIT)#245(BLE/LoRa)  # Maximum size of a data packet in bytes
+    PTx = 543e-3#24e-3(BLE)  # Power consumption
+    PRx = 90e-3#20e-3(BLE)  # Power consumption
+    PIdle = 2.4e-3#5e-3  (BLE)# Power consumption
+    PSleep = 0.015e-3  # 3e-6(BLE)  # Power consumption
+    tsTx = 1.67#0.92857e-3#(Lora)#(2193e-3)*245/100#1080e-6(BLE)  # Transmission time in seconds
+    tsRx = 0.92857e-3#(Lora)#400e-6#400e-6  (BLE)# Reception time in seconds
+    tsIdle = 10#1080e-6  # Idle time in seconds
+    ton = tsTx + tsRx + tsIdle
+    tsSleep = ta - ton  # Sleep time in seconds
+    print(tsSleep)
+    PER = 0.01  # Packet error rate
+    PSleepR = 0#0.001408e-3#4.3e-6#3e-6  # Power consumption during sleep mode for reception in watts
+    synchronization_on_data_frame_possible = 0  # Boolean indicating if synchronization on data frame is possible
+    Esyn = 0  # Energy consumed for synchronization in joules
+    Ebattery = 13000  # Initial energy level of the battery in joules
+    total_time = Lifetime_cal(ta, tsyn, sa_list[i], MaxDataPacketSize, PTx, PRx, PIdle, PSleep, PER, tsTx, tsRx, tsIdle,
+                              tsSleep, ton, PSleepR, synchronization_on_data_frame_possible, Esyn, Ebattery)
+    total_time_inyear_NBIOT= total_time / (3600 * 24 * 365)
+   # print(
+     #   f"Total time in {'year' if total_time_inyear >= 1 else 'day'}s: {total_time_inyear if total_time_inyear >= 1 else total_time / (3600 * 24)}")
+    total_time_inyear_list_NBIoT.append(total_time_inyear_NBIOT)'''
+
+
+
+"""LORA
+Macpl=250+13
+NbSymbPreamble=12.25
+Tsymb= 32768
+numerator=2100
+denominator=48
+NySymbol=212
+packet_time time=7755093e-3ms
+idle time=1s
+rxtime=7755093e-3ms
+419.604mW in Tx 
+	44.064mW in Rx 
+	7.104mW for cpu On 
+	0.001408mW in sleep mode
+	
+	
+#print("ta_list =", ta_list)
+#print("total_time_year_list =", total_time_inyear_list)
+	
+"""
